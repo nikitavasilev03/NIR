374c6bd1edafa4fbcf1bf00a0ea34193a6cb33db
Никита Васильев <nikitavasilev2303@gmail.com>
2019-04-21 21:50:47 +0300

    Добавлен консольный проект Audio, для тестирования

---
+++ Audio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NAudio.Wave;
+
+using static PL.Console.Out;
+
+namespace Audio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
