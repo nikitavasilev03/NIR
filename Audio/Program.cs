88d3f14b7cbb650174053bea4d6c667869cb7e1b
Никита Васильев <nikitavasilev2303@gmail.com>
2019-04-21 23:20:26 +0300

    add

--- Audio/Program.cs
+++ Audio/Program.cs
@@ -14,14 +14,16 @@ namespace Audio
     {
         static void Main(string[] args)
         {
-            //using (WaveFileReader reader = new WaveFileReader("Test1.wav"))
-            //{
-            //    do
-            //    {
-            //        byte x = reader.Re
-            //    } while
-            //}
-            Print(prepare("Test1.wav"));
+            byte[] buffer;
+            using (WaveFileReader reader = new WaveFileReader("Test1.wav"))
+            {
+                buffer = new byte[reader.Length];
+                reader.Read(buffer, 0, buffer.Length);
+                reader.Close();
+            }
+            double[] dataAplituds = readAmplitudeValues(buffer);
+
+            prepare("Test1.wav");
         }
         public static double[] prepare(string wavePath)
 
@@ -37,6 +39,35 @@ namespace Audio
             for (int i = 0; i < data.Length; i++)
             {
                 data[i] = (BitConverter.ToInt32(wave, 44 + i * 4)) / 4294967296.0;
+            }
+
+            return data;
+        }
+
+        public static double[] readAmplitudeValues(byte[] buffer, bool isBigEndian = true)
+        {
+            int MSB, LSB; // старший и младший байты
+            double[] data = new double[buffer.Length / 2];
+
+            for (int i = 0; i < data.Length; i += 2)
+            {
+                if (isBigEndian) // задает порядок байтов во входном сигнале
+                {
+                    // первым байтом будет MSB
+                    MSB = buffer[2 * i];
+                    // вторым байтом будет LSB
+                    LSB = buffer[2 * i + 1];
+                }
+                else
+                {
+                    // наоборот
+                    LSB = buffer[2 * i];
+                    MSB = buffer[2 * i + 1];
+                }
+                // склеиваем два байта, чтобы получить 16-битное вещественное число
+                // все значения делятся на максимально возможное - 2^15
+                double x = (BitConverter.ToInt32(buffer, i * 2)); // 4294967296.0;
+                data[i] = ((MSB << 8) | LSB); // 32768.0;
             }
 
             return data;
