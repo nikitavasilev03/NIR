14b8e1c1373a6405f4c22dbbe5a95e4028111a7f
Никита Васильев <nikitavasilev2303@gmail.com>
2019-04-21 22:39:32 +0300

    Add reading

--- Audio/Program.cs
+++ Audio/Program.cs
@@ -14,7 +14,32 @@ namespace Audio
     {
         static void Main(string[] args)
         {
-            
+            //using (WaveFileReader reader = new WaveFileReader("Test1.wav"))
+            //{
+            //    do
+            //    {
+            //        byte x = reader.Re
+            //    } while
+            //}
+            Print(prepare("Test1.wav"));
+        }
+        public static double[] prepare(string wavePath)
+
+        {
+            double[] data;
+            byte[] wave;
+            System.IO.FileStream WaveFile = System.IO.File.OpenRead(wavePath);
+            wave = new byte[WaveFile.Length];
+            data = new double[(wave.Length - 44) / 4];//shifting the headers out of the PCM data;
+            WaveFile.Read(wave, 0, Convert.ToInt32(WaveFile.Length));//read the wave file into the wave variable
+
+                                                                     /***********Converting and PCM accounting***************/
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (BitConverter.ToInt32(wave, 44 + i * 4)) / 4294967296.0;
+            }
+
+            return data;
         }
     }
 }
